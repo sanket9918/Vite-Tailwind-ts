a463a3d2b1a3f997ce96a9167d6441c60d19fa98
Sanket Mohapatra <www.sanket1mohapatra@gmail.com>
2021-04-10 14:25:15 +0530

    PWA added

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,17 @@
 import { render } from 'preact'
 import App from './app'
 import './style/index.css'
+import { registerSW } from 'virtual:pwa-register'
 
+
+const updateSW = registerSW({
+    onNeedRefresh() {
+        confirm("We need a refresh")
+    },
+    onOfflineReady() {
+        alert('You are now offline.We are working from cache.')
+    }
+})
+
+updateSW()
 render(<App />, document.getElementById('app')!)
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,34 @@
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import preact from '@preact/preset-vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [preact()]
+  plugins: [preact(),
+  VitePWA({
+    mode: 'development',
+    base: '/',
+    manifest: ({
+      "name": 'PoseDetect',
+      "short_name": 'PoseDetect',
+      "start_url": "/",
+      "display": "standalone",
+      "orientation": "portrait",
+      "background_color": "#fff",
+      "theme_color": "#673ab8",
+      "icons": [
+        {
+          "src": "/assets/icons/android-chrome-192x192.png",
+          "type": "image/png",
+          "sizes": "192x192"
+        },
+        {
+          "src": "/assets/icons/android-chrome-512x512.png",
+          "type": "image/png",
+          "sizes": "512x512"
+        }
+      ]
+    })
+  }
+  )]
 })
